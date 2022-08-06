2dd4b9b1b43f9e2419e22cc3629cb473367e69d7
RakeshUP <parthiban.rakesh95@gmail.com>
2022-08-06 22:16:59 +0530

    GA4 added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,24 @@ import 'inter-ui/inter.css'
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-NWPYGWKR9Y" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NWPYGWKR9Y');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
